367c1b9101abcd0a4bcc9756fb13c0a173ea6127
Rudolf Olah <omouse@gmail.com>
2013-06-22 21:46:46 -0400

    login page accepts demo/demo user account #1

--- struts-bank/src/com/goaugust/strutsbank/LoginAction.java
+++ struts-bank/src/com/goaugust/strutsbank/LoginAction.java
@@ -1,20 +1,25 @@
 package com.goaugust.strutsbank;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class LoginAction extends Action {
 
-	@Override
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-		return mapping.findForward("success");
-	}
-	
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+                                 HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (!username.equals("demo") || !password.equals("demo")) {
+            return mapping.findForward("errors");
+        }
+        return mapping.findForward("success");
+    }
+
 }
